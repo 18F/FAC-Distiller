dc9eb3f19294b322157764dab152c80172e65237
Daniel Naab <dnaab@flexion.us>
2020-08-13 15:48:47 -0500

    Produce CFDA prefixes from CFDA table rather than AssistanceListing, so we can pick up non-standard prefixes that are found in the FAC (CFDAs containing letters).

--- distiller/data/management/commands/print_agency_prefixes.py
+++ distiller/data/management/commands/print_agency_prefixes.py
@@ -4,19 +4,28 @@ findings, corrective action plans, and keywords from uploaded PDFs in
 S3.
 """
 
+import re
 from decimal import Decimal
 
 from django.core.management.base import BaseCommand
 
-from ...models import AssistanceListing
+from ...models import CFDA
 
 
 class Command(BaseCommand):
     help = "Prints active CFDA prefixes of the form XX.X"
 
     def handle(self, *args, **options):
-        cfdas = AssistanceListing.objects.all().order_by(
-            'program_number').values_list('program_number', flat=True)
-        prefixes = set('%04.1f' % Decimal(cfda) for cfda in cfdas)
+        # Use a regex to match the parts, because the FAC has some CFDAs
+        # with letters - eg, "84.U42" and "97.IPA"
+        cfda_regex = re.compile('(\d\d)\.(.*)')
+        prefixes = set()
+
+        cfdas = CFDA.objects.all().values_list('cfda', flat=True)
+        for cfda in cfdas:
+            match = cfda_regex.match(cfda)
+            prefix = f'{match.group(1)}.{match.group(2)[0]}'
+            prefixes.add(prefix)
+
         for prefix in sorted(prefixes):
             print(prefix)
